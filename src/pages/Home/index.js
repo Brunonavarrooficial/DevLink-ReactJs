b1f720f680050d7477ea3f4f0a900195ebe1d2a0
Brunonavarrooficial <bnacessoria@gmail.com>
2022-11-07 18:28:53 -0300

    Styled Home Init

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,27 @@
-export default function Home(){
-    return(
-        <div>
-            <h1>Página Home</h1>
+import './home.css';
+
+export default function Home() {
+    return (
+        <div className="home-container">
+            <h1>Bruno Navarro Oficial</h1>
+            <span>Veja Meus Links 👇</span>
+            <main className="link">
+                <section className="link-area">
+                    <a href="#">
+                        <p className="link-text">Canal no Youtube</p>
+                    </a>
+                </section>
+                <section className="link-area">
+                    <a href="#">
+                        <p className="link-text">Perfil Linkdln</p>
+                    </a>
+                </section>
+                <section className="link-area">
+                    <a href="#">
+                        <p className="link-text">Perfil Instagram</p>
+                    </a>
+                </section>
+            </main>
         </div>
     )
 }
