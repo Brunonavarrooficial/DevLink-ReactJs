7273571f6612aefd7ef06aeb108dc2030bcef8e0
Brunonavarrooficial <bnacessoria@gmail.com>
2022-11-08 18:21:20 -0300

    Styled home and socialmedia pages

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,9 @@
 import './home.css';
 
+import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa';
+
+import { Social } from '../../components/Social';
+
 export default function Home() {
     return (
         <div className="home-container">
@@ -7,20 +11,31 @@ export default function Home() {
             <span>Veja Meus Links 👇</span>
             <main className="link">
                 <section className="link-area">
-                    <a href="#">
+                    <a href="https://www.instagram.com/p/CjgockuOsEO/">
                         <p className="link-text">Canal no Youtube</p>
                     </a>
                 </section>
                 <section className="link-area">
-                    <a href="#">
+                    <a href="https://www.instagram.com/p/CjgockuOsEO/">
                         <p className="link-text">Perfil Linkdln</p>
                     </a>
                 </section>
                 <section className="link-area">
-                    <a href="#">
+                    <a href="https://www.instagram.com/p/CjgockuOsEO/">
                         <p className="link-text">Perfil Instagram</p>
                     </a>
                 </section>
+                <footer>
+                    <Social url="https://www.instagram.com/mamaefalei">
+                        <FaFacebook size={35} color="#fff" />
+                    </Social>
+                    <Social url="https://www.instagram.com/mbl">
+                        <FaYoutube size={35} color="#fff" />
+                    </Social>
+                    <Social url="https://www.instagram.com/brunonavarrooficial">
+                        <FaInstagram size={35} color="#fff" />
+                    </Social>
+                </footer>
             </main>
         </div>
     )
